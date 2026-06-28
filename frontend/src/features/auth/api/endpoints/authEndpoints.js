2f750883aa780e682da2130df3f297c557e42d07
Mohamed Mahmoud <m.mahmoud.alsaid.official@gmail.com>
2026-06-28 18:22:24 +0300

    refactor: reduce auth pages into only three (login, register and verify code) and some changes

--- frontend/src/features/auth/api/endpoints/authEndpoints.js
+++ frontend/src/features/auth/api/endpoints/authEndpoints.js
@@ -1,12 +1,5 @@
 const AuthEndpoints = {
-    me: `/auth/me`,
-    login: `/auth/login`,
-    refresh: `/auth/refresh`,
-    register: `/auth/register`,
-    forgetPassword: `/auth/forget-password`,
-    resendOTP: `/auth/resend-otp`,
-    resetPassword: `/auth/reset-password`,
-    verifyEmail: `/auth/verify-email`,
+    
 };
 
 export default AuthEndpoints;
--- frontend/src/features/auth/components/ui/formSubtitle.jsx
+++ frontend/src/features/auth/components/ui/formSubtitle.jsx
@@ -3,8 +3,7 @@ function FormSubtitle({ type }) {
 
     const subTitle = type === 'login' ? 'Please enter your details to sign in to your account.'
         : type === 'register' ? 'Get started by creating your account in just a few steps.' :
-            type === 'forget' ? `Enter your email address and we'll send you a link to reset your password.` :
-                type === 'reset' ? 'We sent a 6-digit verification code to your email. Enter it below to proceed.' : '';
+            type === 'verify' ? 'We sent a 6-digit verification code to your email. Enter it below to proceed.' : '';
 
     return (
         <p className='text-muted-foreground text-txt-sm md:text-txt-md lg:text-txt-lg'>
