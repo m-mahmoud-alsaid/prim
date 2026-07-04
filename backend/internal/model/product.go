89648ed840fa60366a9f1beeb8a9bcdbbe11ad48
Amr Ali <amrrmohamed34342@gmail.com>
2026-07-05 01:08:18 +0300

    refactor(product): following DDD practice(an object creation is service responsiblity)

--- backend/internal/model/product.go
+++ backend/internal/model/product.go
@@ -6,57 +6,104 @@ import (
 	"github.com/google/uuid"
 )
 
+type Brand struct {
+	ID uuid.UUID
+
+	Name    string
+	LogoURL string
+	LogoAlt string
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
+}
+
 type Tag struct {
+	ID uuid.UUID
+
+	Name string
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
+}
+
+type ProductMedia struct {
 	ID        uuid.UUID
-	Name      string
-	DeletedAt time.Time
+	VariantID uuid.UUID
+
+	URL      string
+	Alt      string
+	MimeType string
+
+	// the first media always the main
+	SortOrder int
+
+	Width    int
+	Height   int
+	FileSize int64
+
+	CreatedBy uuid.UUID
+	UpdatedBy uuid.UUID
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
+}
+
+type ProductCategory struct {
+	ID uuid.UUID
+
+	Name string
+	Slug string
+
+	ParentID *uuid.UUID
+
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
-func NewTag(name string) *Tag {
-	now := time.Now().UTC()
-	return &Tag{
-		ID:        uuid.New(),
-		Name:      name,
-		CreatedAt: now,
-		UpdatedAt: now,
-	}
+type ProductVariantStatus string
+
+const (
+	ProductVariantActive       ProductVariantStatus = "active"
+	ProductVariantInactive     ProductVariantStatus = "inactive"
+	ProductVariantDiscontinued ProductVariantStatus = "discontinued"
+)
+
+type ProductVariant struct {
+	ID        uuid.UUID
+	ProductID uuid.UUID
+
+	SKU      string
+	Slug     string
+	Status   ProductVariantStatus
+	Price    int64
+	Currency string
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
 }
 
+type ProductStatus string
+
+const (
+	ProductStatusActive   string = "active"
+	ProductStatusDraft    string = "draft"
+	ProductStatusArchived string = "archived"
+)
+
 type Product struct {
-	ID               uuid.UUID
+	ID      uuid.UUID
+	BrandID uuid.UUID
+
 	Title            string
 	ShortDescription string
 	Description      string
-	SKU              string
-	Slug             string
-	Status           string
-	Price            int64
-	Currency         string
-	DeletedAt        time.Time
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
-}
+	Status           ProductStatus
 
-func NewProduct(
-	title,
-	shortDescription,
-	description,
-	sku,
-	slug,
-	status string,
-	price int64,
-	currency string,
-) *Product {
-	return &Product{
-		Title:            title,
-		ShortDescription: shortDescription,
-		Description:      description,
-		SKU:              sku,
-		Slug:             slug,
-		Status:           status,
-		Price:            price,
-		Currency:         currency,
-	}
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
 }
