173c965085f678b007041a907978b838d3e9fae6
Mohamed Mahmoud <m.mahmoud.alsaid.official@gmail.com>
2026-07-21 00:33:29 +0300

    style: complete product card and product grid

--- frontend/src/features/home/components/ui/productsCard.jsx
+++ frontend/src/features/home/components/ui/productsCard.jsx
@@ -1,18 +1,24 @@
 import Stars from "@/features/home/components/ui/stars";
 import CustomButton from "@/components/ui/button";
+import { Heart } from "lucide-react";
 
 export default function ProductsCard({ cardDetails }) {
 	return (
 		<div className="cursor-pointer hover:scale-95 border border-border rounded-md overflow-hidden">
-			<div className="aspect-auto">
+			<div className="relative aspect-auto">
 				<img
 					src={cardDetails.img}
 					alt=""
 					className="object-center object-cover w-full h-full"
 				/>
+				<div className="absolute flex items-center justify-center top-2.5 right-2.5 group rounded-full w-8 h-8 md:w-10 md:h-10 bg-background text-foreground">
+					<Heart className="group-hover:scale-110" />
+				</div>
 			</div>
 			<div className="p-2">
-				<p className="font-medium mb-1">{cardDetails.product}</p>
+				<p className="font-medium mb-1 text-foreground">
+					{cardDetails.product}
+				</p>
 				<p className=""></p>
 				<p className="flex items-center gap-2.5 mb-2.5">
 					<span className="">
@@ -23,7 +29,7 @@ export default function ProductsCard({ cardDetails }) {
 					</span>
 				</p>
 				<p className="flex gap-2.5 items-center">
-					<span className="font-medium text-title-sm md:text-title-md">
+					<span className="font-medium text-title-sm md:text-title-md text-foreground">
 						{cardDetails.price}
 					</span>
 					<span className="">
@@ -36,7 +42,7 @@ export default function ProductsCard({ cardDetails }) {
 					</span>
 				</p>
 			</div>
-			<div className="h-10 m-2.5 text-primary-foreground bg-primary rounded-md">
+			<div className="h-10 m-2.5 text-primary-foreground bg-primary rounded-md hover:bg-accent hover:text-accent-foreground">
 				<CustomButton text="Add to cart" />
 			</div>
 		</div>
