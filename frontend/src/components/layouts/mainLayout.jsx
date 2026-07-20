a491014959eb0cbe1e445b06fa0e03342acb904f
Mohamed Mahmoud <m.mahmoud.alsaid.official@gmail.com>
2026-07-20 21:40:52 +0300

    refactor: make the main layout better. there is content? the footer appears when scrolling. no content? the footer stay at the bottom of the page

--- frontend/src/components/layouts/mainLayout.jsx
+++ frontend/src/components/layouts/mainLayout.jsx
@@ -2,11 +2,11 @@ import { Header, Footer } from "@/components/ui";
 
 function MainLayout({ children }) {
 	return (
-		<>
+		<div className="flex flex-col min-h-screen">
 			<Header />
-			<div className="p-2.5 md:p-5">{children}</div>
+			<div className="p-2.5 md:p-5 flex-1">{children}</div>
 			<Footer />
-		</>
+		</div>
 	);
 }
 
--- frontend/src/components/ui/header.jsx
+++ frontend/src/components/ui/header.jsx
@@ -1,26 +1,25 @@
-import { Categories } from '@/components/ui/categories'
-import { SearchBar } from '@/components/ui/search'
-import { HeaderActions } from '@/components/ui/headerActions'
+import { Categories } from "@/components/ui/categories";
+import { SearchBar } from "@/components/ui/search";
+import { HeaderActions } from "@/components/ui/headerActions";
 
 export function Header() {
-
-    return (
-        <div className='sticky top-0 z-50 text-foreground bg-background/70 backdrop-blur-3xl'>
-            <div className='grid gap-5 grid-cols-3 p-5 pr-2.5 pl-2.5 border-b border-border-color'>
-                <h1 className='col-span-3 md:col-span-1 md:justify-self-start font-black text-title-sm md:text-title-md lg:text-title-lg text-center'>
-                    <span className=''>PRI</span>
-                    <span className='text-orange-500'>M</span>
-                </h1>
-                <div className='col-span-3 md:col-span-1'>
-                    <SearchBar />
-                </div>
-                <div className='col-span-3 md:col-span-1 justify-self-end flex justify-end items-center gap-5 text-foreground'>
-                    <HeaderActions />
-                </div>
-            </div>
-            <div className='p-5 pr-2.5 pl-2.5'>
-                <Categories />
-            </div>
-        </div>
-    )
+	return (
+		<div className="sticky top-0 z-50 text-foreground bg-background/70 backdrop-blur-3xl">
+			<div className="grid gap-5 grid-cols-3 p-5 pr-2.5 pl-2.5 border-b border-border-color">
+				<h1 className="col-span-3 md:col-span-1 md:justify-self-start font-medium text-title-sm md:text-title-md lg:text-title-lg text-center">
+					<span className="">PRI</span>
+					<span className="text-orange-500">M</span>
+				</h1>
+				<div className="col-span-3 md:col-span-1">
+					<SearchBar />
+				</div>
+				<div className="col-span-3 md:col-span-1 justify-self-end flex justify-end items-center gap-5 text-foreground">
+					<HeaderActions />
+				</div>
+			</div>
+			<div className="p-5 pr-2.5 pl-2.5">
+				<Categories />
+			</div>
+		</div>
+	);
 }
