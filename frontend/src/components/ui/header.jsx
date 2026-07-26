e82b95f03a0020d7214244aaccfb655d4f2a421a
Mohamed Mahmoud <m.mahmoud.alsaid.official@gmail.com>
2026-07-26 20:01:56 +0300

    style: some fixes in component's style

--- frontend/src/components/ui/header.jsx
+++ frontend/src/components/ui/header.jsx
@@ -6,9 +6,9 @@ export function Header() {
 	return (
 		<div className="sticky top-0 z-50 text-foreground bg-background/70 backdrop-blur-3xl">
 			<div className="grid gap-5 grid-cols-3 p-5 pr-2.5 pl-2.5 border-b border-border-color">
-				<h1 className="col-span-3 md:col-span-1 md:justify-self-start font-medium text-title-sm md:text-title-md lg:text-title-lg text-center">
+				<h1 className="col-span-3 md:col-span-1 md:justify-self-start font-black text-title-sm md:text-title-md lg:text-title-lg text-center">
 					<span className="">PRI</span>
-					<span className="text-orange-500">M</span>
+					<span className="text-accent-brand">M</span>
 				</h1>
 				<div className="col-span-3 md:col-span-1">
 					<SearchBar />
--- frontend/src/features/home/components/ui/sectionTitle.jsx
+++ frontend/src/features/home/components/ui/sectionTitle.jsx
@@ -6,7 +6,7 @@ export default function SectionTitle({ title }) {
 			<p className="capitalize font-medium text-title-sm md:text-title-md text-foreground">
 				{title}
 			</p>
-			<p className="flex gap-5 items-center text-txt-sm md:text-txt-md lg:text-txt-lg">
+			<p className="flex gap-5 items-center text-txt-sm md:text-txt-md lg:text-txt-lg cursor-pointer text-accent-brand hover:underline hover:underline-offset-4">
 				<span className="">See all</span>
 				<span className="">
 					<HiMiniArrowLongRight />
